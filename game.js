d2cb0a57912bae470774f1fc361378b7e5806078
FuuX <43573177+FuuX@users.noreply.github.com>
2022-08-19 21:41:54 +0200

    created function to get computers choice

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
+function getComputerChoice() {
+    let options = ["rock", "paper", "scissors"];
+    let choice = options[Math.floor(Math.random() * 3)]
+    return choice;
+}
+
+
 function playRound(playerSelection, computerSelection) {
   }
    
   const playerSelection = "rock";
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
+
