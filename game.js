89e07a52daa357024019f07ee3daf3896587f88d
FuuX <43573177+FuuX@users.noreply.github.com>
2022-08-19 21:27:53 +0200

    main code structure

---
+++ game.js
@@ -0,0 +1,6 @@
+function playRound(playerSelection, computerSelection) {
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
